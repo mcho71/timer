e9450c8c4d77eaa8b4cca71d76d99979d6db3e5a
MamoruMachida <machida.city7110@gmail.com>
2020-04-20 17:08:48 +0900

    Routerを削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomIconRegistry, SVG_ICONS } from './shared/custom-icon-registry';
 import { SharedModule } from './shared/shared.module';
@@ -104,7 +103,6 @@ export const svgIconProviders = [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule,
     SharedModule,
