a9e9224785499b66259dacabc7848353696b7778
MamoruMachida <machida.city7110@gmail.com>
2020-04-20 17:07:14 +0900

    レイアウトを整える

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'timer';
+  mode: 'over' | 'side' = 'side';
+  isOpened = true;
+  isSideBySide = true;
+  @ViewChild(MatSidenav, { static: true })
+  sideNav: MatSidenav;
+  private sideBySideWidth = 992;
+  isTransitioning = false;
+
+  ngOnInit() {
+    // Do not initialize the search on browsers that lack web worker support
+    if ('Worker' in window) {
+      // Delay initialization by up to 2 seconds
+      console.log(window.Worker);
+    }
+
+    this.onResize(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event.target.innerWidth'])
+  onResize(width: number) {
+    this.isSideBySide = width >= this.sideBySideWidth;
+    if (this.isSideBySide) {
+      this.sideNav.toggle(true);
+    }
+  }
 }
