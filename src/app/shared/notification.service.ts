acf80911b84205b5cb88541687467a5f596f6327
MamoruMachida <machida.city7110@gmail.com>
2020-05-01 16:56:06 +0900

    音を鳴らす
    この音
    https://www.zedge.net/ringtone/24a22f7d-102b-3c04-bbe2-c4c592977c28

--- src/app/shared/notification.service.ts
+++ src/app/shared/notification.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
 
+const isEnable = 'Notification' in window;
+const audio = new Audio('assets/sounds/notification.mp3');
+
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
-
+  private permitted = false;
   constructor() {
-    Notification.requestPermission();
+    if (isEnable) {
+      Notification.requestPermission().then(onfulfilled => this.permitted = onfulfilled == 'granted');
+    }
   }
 
   spawnNotification(title, body) {
-    var n = new Notification(title, {
-      body,
-      icon: 'assets/icons/icon-128x128.png'
-    });
-    setTimeout(n.close.bind(n), 5000);
+    if (!this.permitted) {
+      return;
+    }
+    if (isEnable) {
+      var n = new Notification(title, {
+        body,
+        icon: 'assets/icons/icon-128x128.png'
+      });
+      setTimeout(n.close.bind(n), 5000);
+    }
+    audio.play().then(() => audio.currentTime = 0);
   }
 }
